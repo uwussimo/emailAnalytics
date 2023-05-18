651073a7b87de15cf48a9fcb47508f28efa49dde
Micah Khatri <mkhatri26@my.whitworth.edu>
2023-05-18 02:49:57 -0700

    return image

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -8,6 +8,13 @@ export async function GET(request: Request) {
     id,
     name: 'Product ' + id,
   };
+  const image_url =
+    'https://www.whitworth.edu/cms/media/whitworth/documents/administration/marketing-amp-communications/whitworth-logos/whitworth-logo-horizontal-rgb.png';
 
-  return NextResponse.json({ product });
+  //return image
+
+  const response = await fetch(image_url);
+  return new Response(response.body, {
+    headers: { 'Content-Type': 'image/png' },
+  });
 }
