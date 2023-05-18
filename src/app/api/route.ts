d898152bf2b0eb6d3896b5c4e0d92cf7b2619138
Micah Khatri <mkhatri26@my.whitworth.edu>
2023-05-18 03:00:13 -0700

    return image:send telegram message

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,24 @@
-import { NextResponse } from 'next/server';
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
 
-  const product = {
-    id,
-    name: 'Product ' + id,
-  };
-  const image_url =
-    'https://www.whitworth.edu/cms/media/whitworth/documents/administration/marketing-amp-communications/whitworth-logos/whitworth-logo-horizontal-rgb.png';
+  const message = `Email ${id} has been read by ${request.headers.get(
+    'user-agent'
+  )}, ${request.headers.get('x-real-ip')}`;
+
+  // send telegram message
+  const sendTo = 701469970; // your telegram id
+  const token = process.env.TELEGRAM_TOKEN;
+
+  await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ chat_id: sendTo, text: message }),
+  });
 
   //return image
+  const image_url =
+    'https://www.whitworth.edu/cms/media/whitworth/documents/administration/marketing-amp-communications/whitworth-logos/whitworth-logo-horizontal-rgb.png';
 
   const response = await fetch(image_url);
   return new Response(response.body, {
